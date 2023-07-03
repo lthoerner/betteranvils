f9862af7091c36ce11349dde3d2f3faeac73d127
Eyesonjune18 <lowellashton@gmail.com>
2023-07-03 11:46:10 -0700

    feat: added basic enchantment anvil behavior without safety checks

---
+++ src/main/java/com/lthoerner/betteranvils/EnchantmentUtils.java
@@ -0,0 +1,91 @@
+package com.lthoerner.betteranvils;
+
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EnchantmentUtils {
+    public static Map<Enchantment, Integer> getAllEnchantments(ItemStack item) {
+        if (item == null) {
+            return null;
+        }
+
+        // Get the standard enchantments of the item
+        Map<Enchantment, Integer> allEnchantments = new HashMap<>(item.getEnchantments());
+
+        // Get the stored enchantments of the item if it is an enchanted book
+        // (Only enchanted books have an EnchantmentStorageMeta)
+        ItemMeta meta = item.getItemMeta();
+        if (meta instanceof EnchantmentStorageMeta) {
+            EnchantmentStorageMeta enchantmentMeta = (EnchantmentStorageMeta) meta;
+            // Combine the stored enchantments of the book with its standard enchantments
+            allEnchantments.putAll(enchantmentMeta.getStoredEnchants());
+        }
+
+        return allEnchantments;
+    }
+
+    public static ItemStack stripEnchantments(ItemStack item) {
+        if (item == null) {
+            return null;
+        }
+
+        // Strip the standard enchantments of the item
+        item.getEnchantments().keySet().forEach(item::removeEnchantment);
+
+        // Strip the stored enchantments of the item if it is an enchanted book
+        ItemMeta meta = item.getItemMeta();
+        if (meta instanceof EnchantmentStorageMeta) {
+            EnchantmentStorageMeta enchantmentMeta = (EnchantmentStorageMeta) meta;
+            enchantmentMeta.getStoredEnchants().keySet().forEach(enchantmentMeta::removeStoredEnchant);
+            item.setItemMeta(enchantmentMeta);
+        }
+
+        return item;
+    }
+
+    public static void storeEnchantsInBook(ItemStack item) {
+        // Stored enchantments are only relevant for enchanted books
+        if (item == null || item.getType() != Material.ENCHANTED_BOOK) {
+            return;
+        }
+
+        // Get all the enchantments of the item and convert them to stored enchantments
+        EnchantmentStorageMeta enchantmentMeta = (EnchantmentStorageMeta) item.getItemMeta();
+        for (Map.Entry<Enchantment, Integer> entry : item.getEnchantments().entrySet()) {
+            assert enchantmentMeta != null;
+
+            // If the book already has an enchantment, take the higher level
+            int standardLevel = entry.getValue();
+            int resultLevel;
+            if (enchantmentMeta.hasStoredEnchant(entry.getKey())) {
+                int storedLevel = enchantmentMeta.getStoredEnchantLevel(entry.getKey());
+                resultLevel = Math.max(standardLevel, storedLevel);
+            } else {
+                resultLevel = standardLevel;
+            }
+
+            enchantmentMeta.addStoredEnchant(entry.getKey(), resultLevel, true);
+            enchantmentMeta.removeEnchant(entry.getKey());
+        }
+
+        item.setItemMeta(enchantmentMeta);
+    }
+
+    public static int calculateAnvilCost(ItemStack resultItem) {
+        Map<Enchantment, Integer> enchantments = getAllEnchantments(resultItem);
+
+        // The cost of the enchantment is the sum of the levels of all enchantments multiplied by 2
+        int cost = 0;
+        for (Map.Entry<Enchantment, Integer> entry : enchantments.entrySet()) {
+            cost += entry.getValue();
+        }
+
+        return cost * 2;
+    }
+}
